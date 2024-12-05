5cafa75c78d66cb08a7e1f72cb41d1c13b65e889
YE-EUN <musiclove529@naver.com>
2024-12-06 01:17:11 +0900

    [feat/#1]: API fetch 메서드 구현
    - 회원 조회
    - 로그 조회
    - 회원 상태 변경
    - 계좌 상태 변경
    - 카드 상태 변경

---
+++ apis/admin.js
@@ -0,0 +1,92 @@
+"use server";
+
+export const fetchMembers = async (name, phone) => {
+  console.log(name, phone);
+
+  const response = await fetch(
+    `http://localhost:8080/api/v1/admin/members?name=${name}&phone=${phone}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `Authorization=eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MSwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE3MzMzNzY1NTcsImV4cCI6MTczNjAwNDU1N30.wkMCov7qS2ZUC3fm0XZcJlvEDwFdYq6xy145mjuBblA;`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("failed to fetch");
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const fetchLogs = async (startDate, endDate, logLevel) => {
+  const response = await fetch(
+    `http://localhost:8080/api/v1/admin/logs?start=${startDate || ""}&end=${
+      endDate || ""
+    }&level=${logLevel}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `Authorization=eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MSwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE3MzMzNzY1NTcsImV4cCI6MTczNjAwNDU1N30.wkMCov7qS2ZUC3fm0XZcJlvEDwFdYq6xy145mjuBblA;`,
+      },
+    }
+  );
+
+  if (!response.ok) throw new Error("failed to fetch");
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const modifyCardState = async (cardId, state) => {
+  console.log(state);
+
+  const response = await fetch(`http://localhost:8080/api/v1/admin/cards/${cardId}/state`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `Authorization=eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MSwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE3MzMzNzY1NTcsImV4cCI6MTczNjAwNDU1N30.wkMCov7qS2ZUC3fm0XZcJlvEDwFdYq6xy145mjuBblA;`,
+    },
+    body: JSON.stringify({
+      state: state,
+    }),
+  });
+
+  if (!response.ok) throw new Error("failed to modify");
+};
+
+export const modifyAccountState = async (accountId, state) => {
+  const response = await fetch(`http://localhost:8080/api/v1/admin/accounts/${accountId}/state`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `Authorization=eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MSwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE3MzMzNzY1NTcsImV4cCI6MTczNjAwNDU1N30.wkMCov7qS2ZUC3fm0XZcJlvEDwFdYq6xy145mjuBblA;`,
+    },
+    body: JSON.stringify({
+      state: state,
+    }),
+  });
+
+  if (!response.ok) throw new Error("failed to modify");
+};
+
+export const modifyMemberState = async (memberId, state) => {
+  const response = await fetch(`http://localhost:8080/api/v1/admin/members/${memberId}/state`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `Authorization=eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MSwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE3MzMzNzY1NTcsImV4cCI6MTczNjAwNDU1N30.wkMCov7qS2ZUC3fm0XZcJlvEDwFdYq6xy145mjuBblA;`,
+    },
+    body: JSON.stringify({
+      state: state,
+    }),
+  });
+
+  if (!response.ok) throw new Error("failed to modify");
+};
