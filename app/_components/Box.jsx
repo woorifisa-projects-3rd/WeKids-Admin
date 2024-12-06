28987b8f807dd92575f25e173a11d8fc3ceb1928
YE-EUN <musiclove529@naver.com>
2024-12-06 10:19:07 +0900

    [feat/#1]: 홈 화면 버튼 hover 추가

--- app/_components/Box.jsx
+++ app/_components/Box.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Box = ({ children }) => {
   return (
-    <div className="bg-main01 text-white flex justify-center items-center text-B-32 w-full h-full rounded-3xl">
+    <div className="bg-main02 text-white flex justify-center items-center text-B-32 w-full h-full rounded-3xl hover:bg-main01">
       {children}
     </div>
   );
